3953b571f463edbdc78b085fd49562d4c59d86ff
PetrMuzikar <muzikar.petr@volny.cz>
2016-10-25 17:21:03 +0200

    The folder Domains is deleted now.

--- Domains/Rectangle.cpp
+++
@@ -1,7 +0,0 @@
-#include "Rectangle.hpp"
-
-//Rectangle::Rectangle(Num xLow, Num xHigh, Num yLow, Num yHigh) : Domain(2, 0.0), xLow_(xLow), xHigh_(xHigh), yLow_(yLow), yHigh_(yHigh)
-//{
-//    S_ = (xHigh_ - xLow_) * (yHigh_ - yLow_);
-//}
-//
