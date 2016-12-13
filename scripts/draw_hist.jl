095a12af14661eb6a4967f2a056c86aa7055f7bf
PetrMuzikar <muzikar.petr@volny.cz>
2016-12-13 12:00:32 +0100

    For generating a histogram of relative errors, it is possible to select pixels using xrange and yrange.

--- scripts/draw_hist.jl
+++ scripts/draw_hist.jl
@@ -2,48 +2,56 @@
 
 using PyPlot;
 
-maxerr = 0.2;
-nbins = 100;
+isdefined(:maxerr) || (maxerr = 0.2);
+isdefined(:nbins) || (nbins = 100);
+isdefined(:suff) || (suff = "hist.eps");
 
-args = ARGS
+println("maxerr = ", maxerr);
+println("nbins = ", nbins);
+println("suff = ", suff);
 
-num = tryparse(Float64, args[1])
-if !isnull(num)
-    maxerr = get(num)
-    args = args[2:end]
+desc = "";
+if isdefined(:xrange)
+    s = "xrange = $(xrange)\n";
+    print(s);
+    desc *= s;
 end
-num = tryparse(Int64, args[1])
-if !isnull(num)
-    nbins = get(num)
-    args = args[2:end]
-end
 
-println("maxerr = ", maxerr)
-println("nbins = ", nbins)
+if isdefined(:yrange)
+    s = "yrange = $(yrange)\n";
+    print(s);
+    desc *= s;
+end
 
 bins = linspace(-maxerr, maxerr, nbins);
 
 fig = 0
-for f in args
+for f in ARGS
     println("Reading the file $(f).")
     fig += 1;
     p = readdlm(f);
-    r = p[:, end-1];
-    filter!(x -> (abs(x) <= maxerr), r);
-    mu = mean(r);
-    sigma = std(r);
+    r = p[:, [3, 4, 10]];
+    filter!(x -> (abs(x[3]) <= maxerr), r);
+    if isdefined(:xrange)
+        filter!(x -> (xrange[1] <= x[1] <= xrange[2]), r);
+    end
+    if isdefined(:yrange)
+        filter!(x -> (yrange[1] <= x[2] <= yrange[2]), r);
+    end
+    mu = mean(r[:,3]);
+    sigma = std(r[:,3]);
 
     figure(fig);
     clf();
-    plt[:hist](r, bins);
+    plt[:hist](r[:,3], bins);
     gca()[:set_xlim](-maxerr, maxerr);
     xlabel("relative differences");
     ylabel("counts");
     xx = gca()[:get_xlim]()[2]*0.5;
     yy = gca()[:get_ylim]()[2]*0.9;
-    ss = @sprintf("μ = %.4e\nσ = %.4e", mu, sigma)
+    ss = @sprintf("μ = %.4e\nσ = %.4e\n%s", mu, sigma, desc);
     text(xx, yy, ss);
-    epsf = replace(f, r"out.dat$", "hist.eps");
+    epsf = replace(f, r"out.dat$", suff);
     savefig(epsf);
 end
 
